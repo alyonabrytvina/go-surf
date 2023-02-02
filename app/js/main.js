4d980372823c1819c81c6dc7d66f8b5361441632
Alyona <alyona.ivanova.kh@gmail.com>
2023-02-02 12:54:29 +0100

    add new slider

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,27 @@ $(function(){
             ' alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-left.svg"' +
             ' alt="">',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
     });
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-right.svg"' +
+            ' alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-left.svg"' +
+            ' alt="">',
+        asNavFor: '.slider-map',
+    });
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true,
+    })
 });
